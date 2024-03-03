cacde97d8b3e02eac1e950768ea73f1950905f61
colin <csn23@ic.ac.uk>
2024-03-03 00:43:51 +0000

    cleaned up the package.json

--- controllers/pools.js
+++ controllers/pools.js
@@ -1,27 +1,4 @@
 const mysql = require('mysql2/promise');
-const stringHash = require('string-hash');
-const redis = require('redis');
-
-// eslint-disable-next-line no-unused-vars
-const redisResult = async (uni) => {
-  const client = redis.createClient();
-
-  client.on('error', (err) => console.log('Redis Client Error', err));
-
-  await client.connect();
-
-  client.on('connect', () => {
-    console.log('Connected to Redis');
-  });
-
-  let poolNum = await client.get(uni);
-  if (!poolNum) {
-    poolNum = stringHash(uni) % 1;
-    await client.set(uni, poolNum, {NX: true});
-  }
-  await client.quit();
-  return poolNum;
-};
 
 const pools = async (uni = 'National') => {
   let pool;
